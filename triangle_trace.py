5d593aa66501c5c201e1764332fb5eef8d597df8
gilberto <levin@readytheory.com>
2024-02-24 09:12:58 -0600

    Make the trace more interestinger

--- triangle_trace.py
+++ triangle_trace.py
@@ -5,14 +5,39 @@ from pprint import pprint
 import spiller
 import triangles
 
-tri2spill = spiller.Spiller()
-tri2spill.start()
-triangles.tri2(500)
-tri2spill.stop()
-tri2spill.save_spillfile("/tmp/tri2_spill.pickle")
+SPILLFILE = "/tmp/trace_spill.pickle"
 
-del tri2spill
+mytrace = spiller.Spiller()
+mytrace.start()
+result2 = triangles.tri2(3)
+if result2 > 0:
+    result1 = triangles.tri1(3)
+if "result1" not in locals():
+    pprint("wow")
+    exit(1)
+if result1 > result2:
+    result3 = triangles.tri3(3)
+else:
+    result4 = triangles.tri4(3)
+if "result3" in locals():
+    result4 = triangles.tri4(3)
+else:
+    result3 = triangles.tri3(3)
+prev = 0
+for x in 4, 5, 6:
+    count = triangles.tri1(x)
+    if x <= prev:
+        print("Wow again")
+        exit(1)
+    prev = x
+
+
+mytrace.stop()
+mytrace.save_spillfile(SPILLFILE)
+
+
+del mytrace
 
 a_spill = spiller.Spiller()
-a_spill.load_spillfile("/tmp/tri2_spill.pickle")
+a_spill.load_spillfile(SPILLFILE)
 pprint(a_spill.report())
