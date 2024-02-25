c3f338a81c93fb413e30b70b827a361bd8788d54
gilberto <levin@readytheory.com>
2024-02-25 10:14:25 -0600

    Show timings on new code

--- timers.py
+++ timers.py
@@ -4,28 +4,46 @@ from pprint import pprint
 
 
 
-# t1 = timeit("tri1(300)", setup="from triangles import tri1", number=3)
-# t2 = timeit("tri2(300)", setup="from triangles import tri2", number=3)
-# t3 = timeit("tri3(300)", setup="from triangles import tri3", number=3)
-# t4 = timeit("tri4(300)", setup="from triangles import tri4", number=3)
-# print(t1, t2, t3, t4)
+t1 = timeit("tri1(300)", setup="from triangles import tri1", number=300)
+t2 = timeit("tri2(300)", setup="from triangles import tri2", number=300)
+t3 = timeit("tri3(300)", setup="from triangles import tri3", number=300)
+t4 = timeit("tri4(300)", setup="from triangles import tri4", number=300)
+print(t1, t2, t3, t4)
 
 
 SPILLFILE = "/tmp/spill1.pickle"
 from spiller import Spiller
-s = Spiller(include_code=False)
-from triangles import tri1
-
-s.start_spill()
-for i in range(2):
-    tri1(0)
-s.stop_spill()
-s.save_spillfile(SPILLFILE)
-
 from spill_analysis import SpillAnalyser
+from triangles import tri1, tri2, tri3, tri4
 
-a1 = SpillAnalyser(spillfile=SPILLFILE)
+def time_trace(func):
+    s = Spiller(include_code=False)
+    s.start_spill()
+    for i in range(8):
+        func(10)
+    s.stop_spill()
+    s.save_spillfile(SPILLFILE)
+    a = SpillAnalyser(spillfile=SPILLFILE)
+    return(a.line_timings()[1])
 
-pprint(a1.line_timings())
+trace1 = time_trace(tri1)
+trace2 = time_trace(tri2)
+trace3 = time_trace(tri3)
+trace4 = time_trace(tri4)
 
+
+print(f"tri1: tri2 -> timeit: {t1 / t2} trace: {trace1 / trace2}")
+print(f"tri1: tri3 -> timeit: {t1 / t3} trace: {trace1 / trace3}")
+print(f"tri1: tri4 -> timeit: {t1 / t4} trace: {trace1 / trace4}")
+
+
+
+# s = Spiller(include_code=False)
+# s.start_spill()
+# tri4(3)
+# s.stop_spill()
+# s.save_spillfile(SPILLFILE)
+# a = SpillAnalyser(SPILLFILE)
+# print(a.line_timings())
+# pprint(s.report())
 
