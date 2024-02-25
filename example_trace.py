a6c7c6f8122d78e9e6acd05c777534e3e8060cc9
gilberto <levin@readytheory.com>
2024-02-25 09:08:04 -0600

    Change start/stop method name

--- example_trace.py
+++ example_trace.py
@@ -4,15 +4,15 @@ import jinja2
 template_create_spiller = Spiller()
 jinja_snippet = "{% if score > 80 %} That's HUGE {% else %} So TINY {% endif %}"
 
-template_create_spiller.start()
+template_create_spiller.start_spill()
 template = jinja2.environment.Environment().from_string(jinja_snippet)
-template_create_spiller.stop()
-template_create_spiller.start()
+template_create_spiller.stop_spill()
+template_create_spiller.start_spill()
 
 template_render_spiller = Spiller()
-template_render_spiller.start()
+template_render_spiller.start_spill()
 template.render(score=100)
-template_render_spiller.stop()
+template_render_spiller.stop_spill()
 
 print(template_create_spiller.report(), flush=True)
 print("-"*80, flush=True)
